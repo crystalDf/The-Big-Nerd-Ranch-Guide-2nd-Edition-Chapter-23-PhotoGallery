a6fe33737429533bba69a1eea7b40ed9ea7ca186
crystalDf <chendong333@gmail.com>
2015-12-28 19:21:20 +0800

    Chapter 23 PhotoGallery

--- app/src/main/java/com/star/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/star/photogallery/PhotoGalleryFragment.java
@@ -1,16 +1,23 @@
 package com.star.photogallery;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.IOException;
+
 public class PhotoGalleryFragment extends Fragment {
+
+    private static final String TAG = "PhotoGalleryFragment";
+
 
     private RecyclerView mPhotoRecyclerView;
 
@@ -22,6 +29,8 @@ public class PhotoGalleryFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+
+        new FetchItemsTask().execute();
     }
 
     @Nullable
@@ -34,5 +43,19 @@ public class PhotoGalleryFragment extends Fragment {
         mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
 
         return view;
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String result = new FlickrFetchr().getUrlString("http://www.bignerdranch.com");
+                Log.i(TAG, "Fetched contents of URL: " + result);
+            } catch (IOException e) {
+                Log.e(TAG, "Failed to fetch URL: ", e);
+            }
+            return null;
+        }
     }
 }
