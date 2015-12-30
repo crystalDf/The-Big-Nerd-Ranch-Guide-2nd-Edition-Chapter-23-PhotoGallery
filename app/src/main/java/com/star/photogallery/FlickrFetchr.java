95a18dc3eb67ac70dbf0c8968fc620790d185879
crystalDf <chendong333@gmail.com>
2015-12-30 19:29:02 +0800

    Chapter 23 PhotoGallery

--- app/src/main/java/com/star/photogallery/FlickrFetchr.java
+++ app/src/main/java/com/star/photogallery/FlickrFetchr.java
@@ -1,6 +1,9 @@
 package com.star.photogallery;
 
 
+import android.net.Uri;
+import android.util.Log;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,6 +14,17 @@ public class FlickrFetchr {
 
     private static final String TAG = "FlickrFetchr";
     private static final String API_KEY = "03e55c312c15c20d0b02b48dbf58e646";
+
+    private static final String METHOD_KEY = "method";
+    private static final String METHOD_VALUE = "flickr.photos.getRecent";
+    private static final String API_KEY_KEY = "api_key";
+    private static final String API_KEY_VALUE = API_KEY;
+    private static final String FORMAT_KEY = "format";
+    private static final String FORMAT_VALUE = "json";
+    private static final String NO_JSON_CALL_BACK_KEY = "nojsoncallback";
+    private static final String NO_JSON_CALL_BACK_VALUE = "1";
+    private static final String EXTRAS_KEY = "extras";
+    private static final String EXTRAS_VALUE = "url_s";
 
     public byte[] getUrlBytes(String urlSpec) throws IOException {
         URL url = new URL(urlSpec);
@@ -41,5 +55,23 @@ public class FlickrFetchr {
 
     public String getUrlString(String urlSpec) throws IOException {
         return new String(getUrlBytes(urlSpec));
+    }
+
+    public void fetchItems() {
+        try {
+            String url = Uri.parse("https://api.flickr.com/services/rest/")
+                    .buildUpon()
+                    .appendQueryParameter(METHOD_KEY, METHOD_VALUE)
+                    .appendQueryParameter(API_KEY_KEY, API_KEY_VALUE)
+                    .appendQueryParameter(FORMAT_KEY, FORMAT_VALUE)
+                    .appendQueryParameter(NO_JSON_CALL_BACK_KEY, NO_JSON_CALL_BACK_VALUE)
+                    .appendQueryParameter(EXTRAS_KEY, EXTRAS_VALUE)
+                    .build().toString();
+
+            String jsonString = getUrlString(url);
+            Log.i(TAG, "Received JSON: " + jsonString);
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to fetch items", e);
+        }
     }
 }
