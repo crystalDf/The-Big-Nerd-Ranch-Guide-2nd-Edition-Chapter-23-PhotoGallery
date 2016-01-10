04aa927e4f8f29e56d10c30d4f2570529d001c44
crystalDf <chendong333@gmail.com>
2016-01-11 00:55:10 +0800

    Chapter 23 PhotoGallery

---
+++ app/src/main/java/com/star/photogallery/GalleryItem.java
@@ -0,0 +1,14 @@
+package com.star.photogallery;
+
+
+public class GalleryItem {
+
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+
+    @Override
+    public String toString() {
+        return mCaption;
+    }
+}
